c06b26be1e767538b4ed8ea5bdaa4849477c109b
ZairiAimenDz <zairiaimendev@gmail.com>
2021-01-21 11:54:50 +0100

    Added Admin Dashboard Layout And FontAwesome

---
+++ Electronics/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Electronics.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+    }
+}
