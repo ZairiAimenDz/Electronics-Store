8e0df788915316aaaf086df03983ef6ffbf96bad
ZairiAimenDz <zairiaimendev@gmail.com>
2021-01-23 20:26:38 +0100

    Minor Changes To Admin Dashboard

--- Electronics/Pages/Admin/Index.cshtml.cs
+++ Electronics/Pages/Admin/Index.cshtml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,8 +14,40 @@ namespace Electronics.Pages.Admin
     [Authorize(Roles ="Admin")]
     public class IndexModel : PageModel
     {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public IndexModel(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
         public void OnGet()
         {
+        }
+
+        [BindProperty]
+        public IFormFile AnnouncementFile{ get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            UploadFile(AnnouncementFile);
+            return Redirect("/Index");
+        }
+        private void UploadFile(IFormFile file)
+        {
+            if (file is not null) 
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "image");
+                string uniqueFileName = "announcement.png";
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
         }
     }
 }
